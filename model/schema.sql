feef21b0209d54198f483181c8ad3f905add73ab
Giovanni Campagna <gcampagn@cs.stanford.edu>
2016-02-23 10:02:02 -0800

    Revert "Reduce seed data"
    This reverts commit f5052b5ad1b71f7f3e8e9ba13c65584665a15b85.

--- model/schema.sql
+++ model/schema.sql
@@ -176,13 +176,14 @@ UNLOCK TABLES;
 LOCK TABLES `device_class` WRITE;
 /*!40000 ALTER TABLE `device_class` DISABLE KEYS */;
 INSERT INTO `device_class` VALUES
-(8,'org.thingpedia.builtin.omlet','omlet',1,'Omlet Account','Connect your Omlet Account to ThingEngine to enable new social network features!',0,0,0);
+(8,'org.thingpedia.builtin.omlet','omlet',1,'Omlet Account','Connect your Omlet Account to ThingEngine to enable new social network features!',0,0,0),
+(22,'org.thingpedia.builtin.sabrina','sabrina',1,'Sabrina','Your magic assistant, at your service.',0,0,0);
 /*!40000 ALTER TABLE `device_class` ENABLE KEYS */;
 UNLOCK TABLES;
 
 LOCK TABLES `device_class_tag` WRITE;
 /*!40000 ALTER TABLE `device_class_tag` DISABLE KEYS */;
-INSERT INTO `device_class_tag` VALUES ('featured',8);
+INSERT INTO `device_class_tag` VALUES ('featured',8),('featured',22);
 /*!40000 ALTER TABLE `device_class_tag` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -197,7 +198,8 @@ UNLOCK TABLES;
 LOCK TABLES `device_code_version` WRITE;
 /*!40000 ALTER TABLE `device_code_version` DISABLE KEYS */;
 INSERT INTO `device_code_version` VALUES
-(8,0,'{\n  \"auth\": {\n    \"type\": \"oauth2\"\n  },\n  \"types\": [\n    \"online-account\",\n    \"messaging\"\n  ],\n  \"params\": {},\n  \"global-name\": \"omlet\",\n  \"triggers\": {\n    \"newmessage\": {\n      \"args\": [\n        \"feed\",\n        \"type\",\n        \"message\"\n      ], \"schema\": [\"Feed\",\"String\",\"String\"],\n      \"doc\": \"trigger on any new message in the feed\"\n    },\n    \"incomingmessage\": {\n      \"args\": [\n        \"feed\",\n        \"type\",\n        \"message\"\n      ], \"schema\": [\"Feed\",\"String\",\"String\"],\n      \"doc\": \"trigger on any incoming (from other people) message in the feed\"\n    }\n  },\n  \"actions\": {\n    \"send\": {\n      \"args\": [\n        \"feed\",\n        \"type\",\n        \"message\"\n      ], \"schema\": [\"Feed\",\"String\",\"String\"],\n      \"doc\": \"send a message to the feed; type can be text or picture\"\n    }\n  }\n}');
+(8,0,'{\n  \"auth\": {\n    \"type\": \"oauth2\"\n  },\n  \"types\": [\n    \"online-account\",\n    \"messaging\"\n  ],\n  \"params\": {},\n  \"global-name\": \"omlet\",\n  \"triggers\": {\n    \"newmessage\": {\n      \"args\": [\n        \"feed\",\n        \"type\",\n        \"message\"\n      ], \"schema\": [\"Feed\",\"String\",\"String\"],\n      \"doc\": \"trigger on any new message in the feed\"\n    },\n    \"incomingmessage\": {\n      \"args\": [\n        \"feed\",\n        \"type\",\n        \"message\"\n      ], \"schema\": [\"Feed\",\"String\",\"String\"],\n      \"doc\": \"trigger on any incoming (from other people) message in the feed\"\n    }\n  },\n  \"actions\": {\n    \"send\": {\n      \"args\": [\n        \"feed\",\n        \"type\",\n        \"message\"\n      ], \"schema\": [\"Feed\",\"String\",\"String\"],\n      \"doc\": \"send a message to the feed; type can be text or picture\"\n    }\n  }\n}'),
+(22,0,'{\"auth\":{\"type\":\"builtin\"},\"triggers\":{\"listen\":{\"args\":[\"message\"],\"schema\":[\"String\"],\"doc\":\"trigger on any message to Sabrina, except those that have a built-in response\"}},\"actions\":{\"say\":{\"args\":[\"message\"],\"schema\":[\"String\"],\"doc\":\"cause Sabrina to say something\"}}}');
 /*!40000 ALTER TABLE `device_code_version` ENABLE KEYS */;
 UNLOCK TABLES;
 
